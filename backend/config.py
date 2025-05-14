66972a36ab6ce83e29b307f5ec07b212c5913b8a
Gohan3500 <carlosbetancourt419@gmail.com>
2025-05-13 20:39:39 -0400

    Refactor database configuration to use environment variables and add .env.example file

--- backend/config.py
+++ backend/config.py
@@ -2,8 +2,8 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'clave-secreta'
-    MYSQL_HOST = 'localhost'
-    MYSQL_USER = 'root'
-    MYSQL_PASSWORD = ''  # Deja vacío si estás en XAMPP sin contraseña
-    MYSQL_DB = 'chatbot_psicologia'
+    MYSQL_HOST = os.environ.get('MYSQL_HOST') or 'localhost'
+    MYSQL_USER = os.environ.get('MYSQL_USER') or 'root'
+    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD') or ''  # Deja vacío si estás en XAMPP sin contraseña
+    MYSQL_DB = os.environ.get('MYSQL_DB') or 'chatbot_psicologia'
     MYSQL_CURSORCLASS = 'DictCursor'
